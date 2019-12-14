8531fcb9bfc3d533b70471d381ceeda16b577f2d
Matthew Segal <mattdsegal@gmail.com>
2019-12-15 10:22:04 +1100

    init

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+    echo "ERROR: Porject path parameter missing"
+    exit 1
+fi
+PROJ_PATH="$1"
+if [[ ! -d "env" ]]; then
+    virtualenv -p python3 env
+fi
+. ./env/bin/activate
+pip install -r requirements.txt
+pip install -r $1/requirements.txt
+ln -s "$1/data" data
+ln -s "$1/src" src
+ln -s "$1/checkpoints" checkpoints
